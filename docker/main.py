4bdd60c1041e74d1e9647ead8c0a747a61ccc035
Alex Yang <zy5f9@mail.missouri.edu>
2019-02-22 17:04:39 -0600

    well it's not failing..

--- docker/main.py
+++ docker/main.py
@@ -1,12 +1,14 @@
-
 # pylint: disable=import-error,no-name-in-module
 from kubernetes import client, config
-
+import time
 # Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
+config.load_incluster_config()
 
 v1 = client.CoreV1Api()
 print("Listing pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+while 1:
+    time.sleep(3)
