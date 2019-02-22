d8b1ca9d6bcaddd4f83a2afbaf763555cbc73e19
Alex Yang <zy5f9@mail.missouri.edu>
2019-02-22 17:40:32 -0600

    flush issue

--- docker/main.py
+++ docker/main.py
@@ -1,14 +1,17 @@
 # pylint: disable=import-error,no-name-in-module
 from kubernetes import client, config
 import time
-# Configs can be set in Configuration class directly or using helper utility
+import sys
+print("Hello")
 config.load_incluster_config()
 
-v1 = client.CoreV1Api()
 print("Listing pods with their IPs:")
+v1 = client.CoreV1Api()
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 while 1:
+    print('.')
+    # sys.stdout.flush()
     time.sleep(3)
