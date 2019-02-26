9d69cdcf780527690a1d0481497b22feffb368f9
Alex Yang <aleozlx@gmail.com>
2019-02-25 23:05:56 -0600

    +jobApi

--- docker/main.py
+++ docker/main.py
@@ -5,13 +5,17 @@ import sys
 print("Hello")
 config.load_incluster_config()
 
-print("Listing pods with their IPs:")
-v1 = client.CoreV1Api()
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+namespace = 'bluecheese'
+# v1 = client.CoreV1Api()
+jobApi = client.BatchV1Api()
+# ret = v1.list_namespaced_pod('celery-jobq', watch=False)
+# for i in ret.items:
+#     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+job = client.V1Job()
+jobApi.create_namespaced_job(namespace, job)
 
 while 1:
     print('.')
     # sys.stdout.flush()
     time.sleep(3)
+
