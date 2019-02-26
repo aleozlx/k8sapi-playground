ef6ab028cb08938747af6b55feab7a7e84122f1a
Alex Yang <aleozlx@gmail.com>
2019-02-26 00:22:14 -0600

    +job using API

--- docker/main.py
+++ docker/main.py
@@ -1,5 +1,6 @@
 # pylint: disable=import-error,no-name-in-module
 from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 import time
 import sys
 print("Hello")
@@ -11,8 +12,34 @@ jobApi = client.BatchV1Api()
 # ret = v1.list_namespaced_pod('celery-jobq', watch=False)
 # for i in ret.items:
 #     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-job = client.V1Job()
-jobApi.create_namespaced_job(namespace, job)
+
+job_spec = client.V1JobSpec(
+    template=client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(name='test_job'),
+        spec=client.V1PodSpec(
+            containers=[
+                client.V1Container(
+                    name='test',
+                    image='busybox',
+                    command=['hostname']
+                )
+            ],
+            restart_policy='Never'
+        )
+    )
+)
+
+print(job_spec)
+
+# https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1Job.md
+try:
+    jobApi.create_namespaced_job(namespace, client.V1Job(
+        metadata=client.V1ObjectMeta(generate_name='test-job-'),
+        spec=job_spec
+    ), pretty='true')
+except ApiException as e:
+    print('ApiException:', e.reason)
+    print(e.body)
 
 while 1:
     print('.')
